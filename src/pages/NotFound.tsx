9a0eb54544a5543733108368cb6362c6dced0b38
NaveenHiranya <naveenhiranya999@gmail.com>
2025-12-03 08:22:20 +0530

    install Not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="p-6 text-center text-white">
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="mb-4">Oops! The page you are looking for does not exist.</p>
+    <div className="p-6 text-center bg-black h-screen flex flex-col justify-center text-white">
+      <h1 className="text-4xl font-bold mb-4">404 | Page Not Found</h1>
+      <p className="mb-4">Oops! Why are you hear...</p>
 
       <Link
         to="/"
-        className="bg-blue-600 px-4 py-2 rounded-lg text-white font-bold"
+        className="bg-green-600 w-max mx-auto px-4 py-2 rounded-lg text-white font-bold"
       >
         Go Home
       </Link>
